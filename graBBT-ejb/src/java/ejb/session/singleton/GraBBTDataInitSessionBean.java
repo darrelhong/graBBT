1c4e09f319b4ee099af415aefd8335919427dc31
darrelhong <darrel.hong@gmail.com>
2020-02-26 15:26:50 +0800

    very basic listing implementation

--- graBBT-ejb/src/java/ejb/session/singleton/GraBBTDataInitSessionBean.java
+++ graBBT-ejb/src/java/ejb/session/singleton/GraBBTDataInitSessionBean.java
@@ -1,7 +1,12 @@
 package ejb.session.singleton;
 
+import ejb.session.stateless.ListingSessionBeanLocal;
 import ejb.session.stateless.RetailerSessionBeanLocal;
+import entity.Listing;
 import entity.RetailerEntity;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
@@ -20,6 +25,9 @@ import util.exception.UnknownPersistenceException;
 public class GraBBTDataInitSessionBean {
 
     @EJB
+    private ListingSessionBeanLocal listingSessionBean;
+
+    @EJB
     private RetailerSessionBeanLocal retailerSessionBean;
 
     @PersistenceContext(unitName = "graBBT-ejbPU")
@@ -36,8 +44,28 @@ public class GraBBTDataInitSessionBean {
 
     private void initialiseData() {
         try {
-            System.out.println("test");
             retailerSessionBean.createNewRetailer(new RetailerEntity("KOI Thé", "manager", "password"));
+
+            Map<String, BigDecimal> sizeOptions = new HashMap<>();
+            sizeOptions.put("Medium", new BigDecimal(0.00));
+            sizeOptions.put("Large", new BigDecimal(1.10));
+            Map<String, BigDecimal> sugarOptions = new HashMap<>();
+            sugarOptions.put("0%", new BigDecimal(0.00));
+            sugarOptions.put("25%", new BigDecimal(0.00));
+            sugarOptions.put("50%", new BigDecimal(0.00));
+            sugarOptions.put("100%", new BigDecimal(0.00));
+            Map<String, BigDecimal> iceOptions = new HashMap<>();
+            iceOptions.put("Normal Ice", new BigDecimal(0.00));
+            iceOptions.put("No Ice", new BigDecimal(0.00));
+            Map<String, BigDecimal> toppingOptions = new HashMap<>();
+            toppingOptions.put("Golden Bubble", new BigDecimal(0.60));
+            toppingOptions.put("Konjac Jelly", new BigDecimal(1.20));
+            toppingOptions.put("Aloe Vera", new BigDecimal(1.20));
+
+            listingSessionBean.createNewListing(new Listing("Milk Tea", new BigDecimal(3.40), "koimilktea.jpeg", sizeOptions, sugarOptions, iceOptions, toppingOptions));
+            listingSessionBean.createNewListing(new Listing("Cacao Barry", new BigDecimal(4.00), "Made with 100% pure Cacao Powder", "koicacaobarry.jpeg", sizeOptions, sugarOptions, iceOptions, toppingOptions));
+            listingSessionBean.createNewListing(new Listing("Black Tea Macchiato", new BigDecimal(3.30), "koiblackteamacchiato.jpeg"));
+            listingSessionBean.createNewListing(new Listing("Golden Oolong Tea", new BigDecimal(2.70), "koigoldenoolongtea.jpeg"));
 
         } catch (InputDataValidationException | RetailerUsernameExistsException | UnknownPersistenceException ex) {
             ex.printStackTrace();
